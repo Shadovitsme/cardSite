9152830cb5aaa9731157c7d903fdec572620fab8
ukki <iu.cashirskaia@yandex.ru>
2025-05-19 17:51:33 +0300

    header

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,16 @@
+<script setup lang="ts">
+import NavMenuItem from './components/navMenuItem.vue'
+</script>
+
 <template>
-  <div><img /></div>
+  <div class="absolute top-0 left-0 right-0 px-1.5 pb-3 flex justify-between border-b-2 border-yellow-400 ">
+    <img class="mt-3" src="./assets/images/logoSite.png" />
+    <div class="my-auto gap-x-7 flex mr-4 h-full  pt-5">
+      <NavMenuItem label="ГЛАВНАЯ"></NavMenuItem>
+      <NavMenuItem label="Услуги"></NavMenuItem>
+      <NavMenuItem label="ПОЛЕЗНАЯ ИНФОРМАЦИЯ"></NavMenuItem>
+      <NavMenuItem label="БЛОГ"></NavMenuItem>
+      <NavMenuItem label="О НАС"></NavMenuItem>
+    </div>
+  </div>
 </template>
---
+++ src/components/navMenuItem.vue
@@ -0,0 +1,18 @@
+<script setup>
+const props = defineProps({
+  label: {
+    type: String,
+    required: true,
+  },
+  href: {
+    type: String,
+    required: false,
+    default: '#',
+  },
+})
+</script>
+<template>
+  <a :href="props.href">
+    <p class="text-[#424242] text-sm uppercase">{{ props.label }}</p>
+  </a>
+</template>
